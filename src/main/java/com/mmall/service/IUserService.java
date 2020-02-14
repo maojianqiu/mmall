e87d68963b9128bcad29b82b32a6a9d9c24b01d9
lidandan <13803345745@163.com>
2020-02-14 18:07:25 +0800

    project user

--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -4,10 +4,21 @@ import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 
 public interface IUserService {
-    public ServerResponse<User> login(String username, String password) ;
+     ServerResponse<User> login(String username, String password) ;
 
     ServerResponse<String> register(User user);
 
     ServerResponse<String> checkValid(String str,String type);
 
+     ServerResponse selectQuestion(String username);
+
+     ServerResponse<String> checkAnswer(String username,String question,String answer);
+
+    public ServerResponse<String> forgetResetPassword(String username,String passwordNew,String forgetToken);
+
+    public ServerResponse<String> resetPassword(String passwordOld,String passwordNew,User user);
+
+    public ServerResponse<User> updateInformation(User user);
+
+    public ServerResponse<User> getInformation(Integer userId);
 }
